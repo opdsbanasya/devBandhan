f7f8a96001fbd4b78659426f1c200913e7bc633c
DHARM <opdsbanasya@gmail.com>
2025-06-04 20:57:50 +0530

    validating the url, password and email

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -21,10 +22,20 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
+      validate(value){
+        if(!validator.isEmail(value)){
+            throw new Error("Invalid Email");
+        }
+      }
     },
     password: {
       type: String,
       required: true,
+      validate(value){
+        if(!validator.isStrongPassword){
+            throw new Error("Password must have: minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1")
+        }
+      }
     },
     gender: {
       type: String,
@@ -47,6 +58,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         "https://i.pinimg.com/736x/6e/59/95/6e599501252c23bcf02658617b29c894.jpg",
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("invalid URL")
+            }
+        }
     },
     about: {
       type: String,
