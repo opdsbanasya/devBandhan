6a03d0e3d13fde0a1284e2799d3188c6d2d64979
DHARM <opdsbanasya@gmail.com>
2025-06-10 23:39:16 +0530

    GET /user/requests/recieved API & Conneting 2 collections

--- backend/src/models/connectionRequest.js
+++ backend/src/models/connectionRequest.js
@@ -5,6 +5,7 @@ const connectionRequestSchema = new mongoose.Schema(
     fromUserId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
+      ref: "User",
     },
     toUserId: {
       type: mongoose.Schema.Types.ObjectId,
@@ -15,8 +16,8 @@ const connectionRequestSchema = new mongoose.Schema(
       required: true,
       enum: {
         values: ["ignored", "interested", "accepted", "rejected"],
-        message: "{VALUE} not suppeted"
-      }
+        message: "{VALUE} not suppeted",
+      },
     },
   },
   {
@@ -24,15 +25,15 @@ const connectionRequestSchema = new mongoose.Schema(
   }
 );
 
-connectionRequestSchema.index({fromUserId: 1, toUserId: 1});
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
-connectionRequestSchema.pre("save", function(next){
+connectionRequestSchema.pre("save", function (next) {
   const connectionRequest = this;
-  if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
-    throw new Error("You can't send request to self")
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("You can't send request to self");
   }
   next();
-})
+});
 
 const ConnectionRequestModel = mongoose.model(
   "connectionRequest",
--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
+const { userAuth } = require("../middlewares/authMiddleware");
+const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express.Router();
 
@@ -11,6 +13,25 @@ userRouter.get("/feed", async (req, res) => {
     res.send(users);
   } catch (err) {
     res.status(400).send("Something went wrong");
+  }
+});
+
+// GET all requests
+userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const pendingReqeusts = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", "firstName lastName about gender age skills profilePhoto");
+
+    res.json({
+      message: "All connection request fetched successfully",
+      pendingReqeusts,
+    });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
