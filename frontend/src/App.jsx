822276ccc4cef3e26efae18f1e014b025f1627c4
DHARM <opdsbanasya@gmail.com>
2025-06-12 20:48:37 +0530

    project setup for frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Globe } from "./components/magicui/globe";
+
+function App() {
+  return (
+    <div>
+      <h1>Devtinder</h1>
+      <Globe />
+    </div>
+  );
+}
+
+export default App;
