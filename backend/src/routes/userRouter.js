8285844acf13d38de84a2dd0f576502a56c5de29
DHARM <opdsbanasya@gmail.com>
2025-06-12 18:59:31 +0530

    GET /feed API

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -5,17 +5,6 @@ const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express.Router();
 
-// GET /feed API - finding all users
-userRouter.get("/feed", async (req, res) => {
-  try {
-    // finding all users
-    const users = await User.find({});
-    res.send(users);
-  } catch (err) {
-    res.status(400).send("Something went wrong");
-  }
-});
-
 // GET all requests
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
   try {
@@ -71,6 +60,38 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(400).json({ message: err.message });
+  }
+});
+
+// GET /feed API - finding all users
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    // data reading
+    const loggedInUser = req.user;
+
+    // Finding user those are hidden from users feed
+    const connections = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    // Pushing all hidden user from feed in a set
+    const hiddenUserFromFeed = new Set();
+    connections.map((user) => {
+      hiddenUserFromFeed.add(user.fromUserId.toString());
+      hiddenUserFromFeed.add(user.toUserId.toString());
+    });
+
+    // Finding all users except the hidden users and loggedInUser
+    const usersForFeed = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hiddenUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select("firstName lastName about gender age skills profilePhoto");
+
+    res.json({ message: "User feed Data", usersForFeed });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 });
 
