4bb5cb8db4a6efc9a5234edda5c1e98e7b3cbe3b
DHARM <opdsbanasya@gmail.com>
2025-06-13 02:50:07 +0530

    / path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Body from "./Components/Body";
 import Login from "./Components/ui/Login"
 import Signup from "./Components/ui/Signup";
 import Profile from "./Components/ui/Profile";
+import Home from "./Components/ui/Home";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Body />}>
+          <Route path="/" element={<Home   />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
           <Route path="profile" element={<Profile />} />
