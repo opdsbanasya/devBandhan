46d6df9822361242295181bbd6a0eb02847296ed
DHARM <opdsbanasya@gmail.com>
2025-06-15 00:45:44 +0530

    fixing issues with signup validation

--- frontend/src/Components/ui/Signup.jsx
+++ frontend/src/Components/ui/Signup.jsx
@@ -1,64 +1,47 @@
-import React, { useRef, useState } from "react";
-import { BorderBeam } from "../magicui/border-beam";
+import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import {
-  signUpDataValidation,
-  validateSingupContinueData,
-} from "@/utils/validation";
+import signUpDataValidation from "@/utils/validation";
+import axios from "axios";
 
 const Signup = () => {
-  const [isContinue, setIsContinue] = useState(false);
-  const [signupData, setSignupData] = useState({});
+  const firstName = useRef();
+  const lastName = useRef();
+  const email = useRef();
+  const password = useRef();
+  const dateOfBirth = useRef();
 
-  const firstName = useRef(null);
-  const lastName = useRef(null);
-  const email = useRef(null);
-  const password = useRef(null);
-  const dateOfBirth = useRef(null);
-  const domain = useRef(null);
-  const skills = useRef(null);
-  const about = useRef(null);
-  const photoUrl = useRef(null);
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!isContinue) {
-      const validateData = signUpDataValidation({
-        firstName: firstName.current.value,
-        lastName: lastName.current.value,
-        email: email.current.value,
-        password: password.current.value,
-        dateOfBirth: dateOfBirth.current.value,
-      });
+    const signupData = {
+      firstName: firstName.current.value,
+      lastName: lastName.current.value,
+      email: email.current.value,
+      password: password.current.value,
+      dateOfBirth: dateOfBirth.current.value,
+    };
+    const validateData = signUpDataValidation(signupData);
 
-      console.log(validateData);
+    console.log(validateData);
 
-      const isNoError = Object.keys(validateData).length === 0;
-      console.log(isNoError);
-      if (isNoError) {
-        setIsContinue(true);
-      }
-    } else {
-      const continueValidateData = validateSingupContinueData({
-        domain: domain.current ? domain.current.value : "",
-        skills: skills.current ? skills.current.value : "",
-        about: about.current ? about.current.value : "",
-        photoUrl: photoUrl.current ? photoUrl.current.value : "",
-      });
-
-      const isAgainNoError = Object.keys(continueValidateData).length === 0;
-      if (isAgainNoError) {
-        alert("You have been registered");
+    const isNoError = Object.keys(validateData).length === 0;
+    console.log(isNoError);
+    if (isNoError) {
+      try {
+        const res = await axios({
+          method: "post",
+          url: "http://localhost:3000/signup",
+          signupData,
+        });
+        console.log(res);
+      } catch (err) {
+        console.log("ERROR: " + err.message);
       }
     }
   };
 
   return (
-    <section
-      data-theme="black"
-      className="w-screen min-h-[90vh] bg-base-200 grid"
-    >
-      <section className="relative w-full h-full flex items-center overflow-x-hidden">
+    <section data-theme="black" className="w-screen h-[90vh] bg-base-200 grid">
+      <section className="relative w-full min-h-full flex items-center overflow-x-hidden">
         {/* Card 2 - behind */}
         <div className="absolute w-[70%] h-[90%] bg-blue-300 rounded-lg shadow-lg z-0 right-0 overflow-hidden">
           <img
@@ -69,149 +52,92 @@ const Signup = () => {
         </div>
 
         {/* Card 1 - front */}
-        <div className="relative w-[30%] h-[85%] rounded-lg shadow-2xl z-10 transform translate-x-30 bg-base-300">
-          <div className="space-y-5 py-5">
-            <div>
-              <h5 className="tex-xl font-semibold text-center shadow-lg mb-2">
+        <div className="relative w-[30%] h-[85%] rounded-lg shadow-2xl z-10 transform translate-x-30 bg-base-300 py-3">
+          <div className="space-y-4">
+            <div className="px-10">
+              <h5 className="text-xl font-semibold text-center shadow-lg mb-2">
                 Sign up
               </h5>
-              <p className=" text-center">
+              <p className="text-center italic text-sm">
                 Enter your details below to register on <span>DevTinder</span>
               </p>
             </div>
             <div className="">
-              <form action="#" className="">
-                {!isContinue ? (
-                  <div className="flex flex-col px-10 gap-1">
-                    <label htmlFor="firstname" className={"pl-2"}>
-                      Firstname
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Firstname"
-                      id="firstname"
-                      name="firstname"
-                      ref={firstName}
-                      required
-                      className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
-                    />
+              <form action="#" className="flex flex-col px-10 gap-1 relative">
+                <label htmlFor="firstname" className={"pl-2"}>
+                  Firstname
+                </label>
+                <input
+                  type="text"
+                  placeholder="Firstname"
+                  id="firstname"
+                  name="firstname"
+                  ref={firstName}
+                  required
+                  className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
+                />
 
-                    <label htmlFor="lastname" className="pl-2">
-                      Lastname
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="LastName"
-                      id="lastname"
-                      name="lastname"
-                      ref={lastName}
-                      required
-                      className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
-                    />
-                    <label htmlFor="email" className="pl-2">
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      placeholder="Email"
-                      id="email"
-                      name="email"
-                      ref={email}
-                      required
-                      className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
-                    />
+                <label htmlFor="lastname" className="pl-2">
+                  Lastname
+                </label>
+                <input
+                  type="text"
+                  placeholder="LastName"
+                  id="lastname"
+                  name="lastname"
+                  ref={lastName}
+                  required
+                  className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
+                />
+                <label htmlFor="email" className="pl-2">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  id="email"
+                  name="email"
+                  ref={email}
+                  required
+                  className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
+                />
 
-                    <label htmlFor="password" className="pl-2">
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      placeholder="Password"
-                      id="password"
-                      name="password"
-                      ref={password}
-                      required
-                      className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
-                    />
+                <label htmlFor="password" className="pl-2">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  id="password"
+                  name="password"
+                  ref={password}
+                  required
+                  className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
+                />
 
-                    <label htmlFor="dob" className="pl-2">
-                      Date of birth
-                    </label>
-                    <input
-                      type="date"
-                      id="dob"
-                      name="dob"
-                      ref={dateOfBirth}
-                      required
-                      className="text-zinc-500 px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-6 focus-within:border-zinc-400"
-                    />
+                <label htmlFor="dob" className="pl-2">
+                  Date of birth
+                </label>
+                <input
+                  type="date"
+                  id="dob"
+                  name="dob"
+                  ref={dateOfBirth}
+                  required
+                  className="text-zinc-500 px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-6 focus-within:border-zinc-400"
+                />
 
-                    <button
-                      className="px-3 py-2 text-red-500 bg-red-100 w-fit mx-auto rounded-md font-semibold cursor-pointer"
-                      onClick={(e) => {
-                        handleSubmit(e);
-                      }}
-                    >
-                      Continue
-                    </button>
-                  </div>
-                ) : (
-                  <div className="flex flex-col px-10 gap-1">
-                    <label htmlFor="domain" className={"pl-2"}>
-                      Domain
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Domain"
-                      id="domain"
-                      name="domain"
-                      className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
-                    />
-
-                    <label htmlFor="skills" className={"pl-2"}>
-                      Skills
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Skills"
-                      id="skills"
-                      name="skills"
-                      className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
-                    />
-
-                    <label htmlFor="photo" className={"pl-2"}>
-                      Profile Photo
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Profile photo link"
-                      id="photo"
-                      name="photo"
-                      className="px-2 py-2 border border-zinc-500 outline-none rounded-sm mb-3 focus-within:border-zinc-400"
-                    />
-
-                    <label htmlFor="about" className={"pl-2"}>
-                      Bio
-                    </label>
-                    <textarea
-                      type="text"
-                      placeholder="Write about your self"
-                      id="about"
-                      name="about"
-                      className="px-2 py-2 h-32 border border-zinc-500 outline-none rounded-sm mb-6 focus-within:border-zinc-400 resize-none"
-                    />
-
-                    <button
-                      className="px-3 py-2 text-red-500 bg-red-100 w-fit mx-auto rounded-md font-semibold cursor-pointer"
-                      onClick={(e) => handleSubmit(e)}
-                    >
-                      Submit
-                    </button>
-                  </div>
-                )}
+                <div className="flex justify-center">
+                  <button
+                    className="px-3 py-2 text-red-500 bg-red-100 w-fit mx-auto rounded-md font-semibold cursor-pointer"
+                    onClick={(e) => handleSubmit(e)}
+                  >
+                    Submit
+                  </button>
+                </div>
               </form>
             </div>
-            <div className="px-10">
+            <div className="px-10 pb-5">
               <p className="text-sm">
                 Already have an account?{" "}
                 <Link
@@ -223,7 +149,6 @@ const Signup = () => {
               </p>
             </div>
           </div>
-          <BorderBeam />
         </div>
       </section>
     </section>
