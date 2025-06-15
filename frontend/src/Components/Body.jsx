11e61f7b72969d007aac222264ff1adbaed4de01
DHARM <opdsbanasya@gmail.com>
2025-06-15 17:32:44 +0530

    fixing the issue with reloading

--- frontend/src/Components/Body.jsx
+++ frontend/src/Components/Body.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./ui/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./ui/Footer";
+import axios from "axios";
+import { BASE_URL } from "@/utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "@/store/userSlice";
 
 function Body() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
+
+  const handleUserData = async () => {
+    try {
+      const userData = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+
+      console.log(userData);
+      
+      dispatch(addUser(userData?.data?.user));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    if (!user) {
+      handleUserData();
+    }
+  }, []);
+
   return (
     <div className="overflow-x-hidden">
       <Navbar />
