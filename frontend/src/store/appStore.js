9e4f11496590c29181dc1ae1ace04c29cb237806
DHARM <opdsbanasya@gmail.com>
2025-06-15 16:45:33 +0530

    userSlice & reducers

--- frontend/src/store/appStore.js
+++ frontend/src/store/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import signupSlice from "./signupDataSlice";
+import userSliceReducer from "./userSlice";
 
 const appStore = configureStore({
   reducer: {
     signupData: signupSlice,
+    user: userSliceReducer,
   },
 });
 
