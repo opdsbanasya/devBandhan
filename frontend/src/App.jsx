5b437846a6b31b7624d3a7899da300390735552f
DHARM <opdsbanasya@gmail.com>
2025-06-15 16:44:24 +0530

    adding /feed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Body from "./Components/Body";
 import Login from "./Components/ui/Login"
 import Signup from "./Components/ui/Signup";
 import Profile from "./Components/ui/Profile";
+import Feed from "./Components/ui/Feed";
 import Home from "./Components/ui/Home";
 
 function App() {
@@ -11,9 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Body />}>
           <Route path="/" element={<Home   />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="profile" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/feed" element={<Feed />} />
         </Route>
       </Routes>
     </>
---
+++ frontend/src/Components/ui/Feed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <div data-theme="black" className="w-screen min-h-[84vh] bg-base-200">
+      User Feed
+    </div>
+  )
+}
+
+export default Feed;
