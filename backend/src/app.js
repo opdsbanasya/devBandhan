a3d50273b7d91c2a602ee547604aac2310bcc42c
DHARM <opdsbanasya@gmail.com>
2025-06-15 15:40:51 +0530

    setting up cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,16 @@ const authRouter = require("./routes/authRouter");
 const profileRouter = require("./routes/profileRouter");
 const requestRouter = require("./routes/requestRouter");
 const userRouter = require("./routes/userRouter");
+const cors = require("cors");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
