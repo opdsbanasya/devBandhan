83f9d2767f2c56e4b5ab738e8ff42809dcc56831
DHARM <opdsbanasya@gmail.com>
2025-06-17 20:50:12 +0530

    setting up routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./Components/ui/Login";
 import Signup from "./Components/ui/Signup";
 import Profile from "./Components/ui/Profile";
 import Feed from "./Components/ui/Feed";
+import GetStarted from "./Components/ui/GetStarted";
 import Home from "./Components/ui/Home";
 
 function App() {
@@ -11,11 +12,14 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Body />}>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />}> 
+            <Route path="/" element={<Feed />} />
+            <Route path="/connetion/:userId" element={<Profile />} />
+          </Route>
+          <Route path="/get-started" element={<GetStarted />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/feed" element={<Feed />} />
         </Route>
       </Routes>
     </>
