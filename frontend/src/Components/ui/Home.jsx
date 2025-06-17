21befc96f0ff02d2ab39a01de553991930324f5e
DHARM <opdsbanasya@gmail.com>
2025-06-17 23:26:02 +0530

    creating dynamic tabs

--- frontend/src/Components/ui/Home.jsx
+++ frontend/src/Components/ui/Home.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import Feed from "./Feed";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import Connections from "./Connections";
+import { MessageCircle, UserPlus, Users } from "lucide-react";
 
 const Home = () => {
-
-  const [isChat, setIsChat] = useState(true);
+  const [tabName, setTabName] = useState("chat");
+  const navigate = useNavigate();
+  const handleTabs = (tab) => {
+    setTabName(tab);
+    navigate("/?tab=" + tab);
+  };
 
   return (
     <div
@@ -12,27 +18,42 @@ const Home = () => {
       className="w-screen h-[90vh] bg-gradient-to-br from-zinc-900 to-zinc-800 flex"
     >
       <div className="w-1/4 h-full bg-base-200 border-r border-zinc-700 text-white">
-        <div className="flex w-full px-5 gap-4">
+        <div className="flex w-full px-5 gap-4 border-b border-zinc-500 py-5 justify-center">
           <button
-            onClick={() => setIsChat(true)}
-            className={`w-1/2 py-2 text-sm font-semibold transition-colors rounded-b-lg ${
-              isChat
+            onClick={() => handleTabs("chat")}
+            className={`px-5 py-2 text-sm font-semibold transition-colors rounded-lg ${
+              tabName === "chat"
                 ? "bg-blue-500 text-white"
                 : "bg-zinc-800 text-zinc-300 hover:bg-zinc-700"
             }`}
           >
-            Chats
+            <MessageCircle />
           </button>
           <button
-            onClick={() => setIsChat(false)}
-            className={`w-1/2 py-2 text-sm font-semibold transition-colors rounded-b-lg ${
-              !isChat
+            onClick={() => handleTabs("connection")}
+            className={` px-5 py-2 text-sm font-semibold transition-colors rounded-lg ${
+              tabName === "connection"
                 ? "bg-blue-500 text-white"
                 : "bg-zinc-800 text-zinc-300 hover:bg-zinc-700"
             }`}
           >
-            Connections
+            <Users />
           </button>
+          <button
+            onClick={() => handleTabs("request")}
+            className={` px-5 py-2 text-sm font-semibold transition-colors rounded-lg ${
+              tabName === "request"
+                ? "bg-blue-500 text-white"
+                : "bg-zinc-800 text-zinc-300 hover:bg-zinc-700"
+            }`}
+          >
+            <UserPlus />
+          </button>
+        </div>
+        <div>
+          {tabName === "chat" && "Chats"}
+          {tabName === "connection" && <Connections />}
+          {tabName === "request" && "Request"}
         </div>
       </div>
       <div className="w-3/4 h-full bg-transparent flex items-center justify-center relative flex-wrap gap-5">
