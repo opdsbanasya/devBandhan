d954644d5b10fe1f89d3c41d34bd3eac66ffdd64
DHARM <opdsbanasya@gmail.com>
2025-06-17 19:56:45 +0530

    formating

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Body from "./Components/Body";
-import Login from "./Components/ui/Login"
+import Login from "./Components/ui/Login";
 import Signup from "./Components/ui/Signup";
 import Profile from "./Components/ui/Profile";
 import Feed from "./Components/ui/Feed";
@@ -11,7 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Body />}>
-          <Route path="/" element={<Home   />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/profile" element={<Profile />} />
