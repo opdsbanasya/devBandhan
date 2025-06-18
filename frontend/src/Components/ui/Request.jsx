90d3ab0b514f4321d6ecd39f661b46615852063e
DHARM <opdsbanasya@gmail.com>
2025-06-18 18:41:09 +0530

    making API call to review request

--- frontend/src/Components/ui/Request.jsx
+++ frontend/src/Components/ui/Request.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { removeRequest } from "@/store/connectionRequestSlice";
+import { BASE_URL } from "@/utils/constants";
+import axios from "axios";
+import React from "react";
+import { useDispatch } from "react-redux";
 
 const Request = ({ request }) => {
+
+  const dispatch = useDispatch();
+
   const { firstName, lastName, profilePhoto } = request.fromUserId;
+
+  const reviewRequest = async (requestId, status) => {
+    await axios.post(
+      `${BASE_URL}/request/review/${status}/${requestId}`,
+      "",
+      { withCredentials: true }
+    );
+
+    dispatch(removeRequest(requestId));
+  };
 
   return (
     <div className="flex items-center gap-5 py-3 px-5 border-b border-b-zinc-800">
@@ -16,11 +33,21 @@ const Request = ({ request }) => {
         <h2>{`${firstName} ${lastName}`}</h2>
       </div>
       <div className="space-x-4">
-        <button className='cursor-pointer'>✔️</button>
-        <button className='cursor-pointer'>❌</button>
+        <button
+          className="cursor-pointer"
+          onClick={() => reviewRequest(request?._id, "accepted")}
+        >
+          ✔️
+        </button>
+        <button
+          className="cursor-pointer"
+          onClick={() => reviewRequest(request?._id, "rejected")}
+        >
+          ❌
+        </button>
       </div>
     </div>
   );
 };
 
-export default Request
+export default Request;
