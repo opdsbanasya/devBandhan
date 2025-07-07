a9b2b19dc9269979a0e867f274261d7dd24b94be
DHARM <opdsbanasya@gmail.com>
2025-07-07 21:25:42 +0530

    responsive to Navbar

--- frontend/src/Components/ui/Navbar.jsx
+++ frontend/src/Components/ui/Navbar.jsx
@@ -6,6 +6,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { TypingAnimation } from "../magicui/typing-animation";
+import { ShinyButton } from "../magicui/shiny-button";
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
@@ -13,48 +14,53 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const handleLogout = async () => {
-    await axios.post(
-      BASE_URL + "/logout",
-      {},
-      { withCredentials: true }
-    );
+    await axios.post(BASE_URL + "/logout", {}, { withCredentials: true });
     dispatch(removeUser());
-    dispatch(removeFeedData()); 
+    dispatch(removeFeedData());
     navigate("/login");
   };
 
   return (
     <div
       data-theme="black"
-      className="navbar shadow-sm px-20 bg-base-100 h-[10vh]"
+      className="navbar shadow-sm px-5 md:px-10 lg::px-20 bg-base-100 h-[10vh]"
     >
       <div className="flex-1">
-        <Link to={"/"} className=" text-xl">
-          🧑‍💻 Dev Tinder
+        <Link to={"/"} className="">
+          <h1 class="text-xl md:text-3xl font-extrabold text-white tracking-wide animate-fade-in flex items-center gap-2">
+            <span class="bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600 bg-clip-text text-transparent">
+              &lt;/&gt; 
+            </span>
+            <span class="hidden md:block bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600 bg-clip-text text-transparent">
+              DevBandhan
+            </span>
+          </h1>
         </Link>
       </div>
       <div className="flex gap-10">
         {!user && (
-          <div className="flex item-center gap-5">
-            <button className="px-5 py-1 font-semibold rounded-full cursor-pointer border hover:bg-green-100 hover:text-green-500 transition-all duration-500">
+          <div className="flex item-center gap-3 md:gap-5">
+            <ShinyButton className="">
               <Link to={"/signup"}>Sign up</Link>
-            </button>
-            <button className="px-5 py-1 font-semibold rounded-full cursor-pointer border hover:bg-blue-100 hover:text-blue-500 transition-all duration-500">
+            </ShinyButton>
+            <ShinyButton className="">
               <Link to={"/login"}>Login</Link>
-            </button>
+            </ShinyButton>
           </div>
         )}
         {user && (
           <div className="dropdown dropdown-end flex gap-5">
             <div className="flex items-center">
-              <div className="flex items-center gap-2">Welcome, <TypingAnimation>{user?.firstName}</TypingAnimation></div>
+              <div className="flex items-center gap-2">
+                Welcome, <TypingAnimation>{user?.firstName}</TypingAnimation>
+              </div>
             </div>
             <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
-              <div className="w-10 rounded-full">
+              <div className="w-10 rounded-full border-2 border-blue-500">
                 <img
                   alt="Tailwind CSS Navbar component"
                   src={user?.profilePhoto}
@@ -66,16 +72,22 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-14 w-52 p-2 shadow-xl shadow-zinc-900"
             >
               <li>
-                <Link to={`/profile/${user._id}`} className="justify-between text-[14px]">
+                <Link
+                  to={`/profile/${user._id}`}
+                  className="justify-between text-[14px]"
+                >
                   Profile
-                  <span className="badge">New</span>
                 </Link>
               </li>
               <li>
-                <Link to={"/upgrade"} className="text-[14px]">Upgrade</Link>
+                <Link to={"/upgrade"} className="text-[14px]">
+                  Upgrade
+                </Link>
               </li>
               <li>
-                <Link to={"/settings"} className="text-[14px]">Settings</Link>
+                <Link to={"/settings"} className="text-[14px]">
+                  Settings
+                </Link>
               </li>
               <li>
                 <p className="text-[14px]" onClick={handleLogout}>
