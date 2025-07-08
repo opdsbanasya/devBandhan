8668d52963b78334ad3b8ba807d842f19f6951c8
DHARM <opdsbanasya@gmail.com>
2025-07-08 18:50:25 +0530

    responsive to home

--- frontend/src/Components/ui/Home.jsx
+++ frontend/src/Components/ui/Home.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import Connections from "./Connections";
-import { MessageCircle, UserPlus, Users } from "lucide-react";
+import { House, MessageCircle, UserPlus, Users } from "lucide-react";
 import Requests from "./Requests";
 import { useDispatch, useSelector } from "react-redux";
 import { addConnections, addRequests } from "@/store/connectionRequestSlice";
 import { BASE_URL } from "@/utils/constants";
 import axios from "axios";
+import { Dock, DockIcon } from "@/components/magicui/dock";
 
 const Home = () => {
   const [tabName, setTabName] = useState("chat");
@@ -44,54 +45,62 @@ const Home = () => {
     if (tab === "request") {
       getRequests();
     }
+    if (tab === "") {
+      return navigate("/");
+    }
     navigate("/?tab=" + tab);
   };
 
   return (
-    <div
-      data-theme="black"
-      className="w-screen h-[90vh] bg-gradient-to-br from-zinc-900 to-zinc-800 flex"
-    >
-      <div className="w-1/4 h-full bg-base-200 border-r border-zinc-700 text-white">
-        <div className="flex w-full px-5 gap-4 border-b border-zinc-500 py-5 justify-center">
-          <button
-            onClick={() => handleTabs("chat")}
-            className={`px-5 py-2 text-sm font-semibold transition-colors rounded-lg ${
-              tabName === "chat"
-                ? "bg-blue-500 text-white"
-                : "bg-zinc-800 text-zinc-300 hover:bg-zinc-700"
-            }`}
-          >
-            <MessageCircle />
-          </button>
-          <button
-            onClick={() => handleTabs("connection")}
-            className={` px-5 py-2 text-sm font-semibold transition-colors rounded-lg ${
-              tabName === "connection"
-                ? "bg-blue-500 text-white"
-                : "bg-zinc-800 text-zinc-300 hover:bg-zinc-700"
-            }`}
-          >
-            <Users />
-          </button>
-          <button
-            onClick={() => handleTabs("request")}
-            className={` px-5 py-2 text-sm font-semibold transition-colors rounded-lg ${
-              tabName === "request"
-                ? "bg-blue-500 text-white"
-                : "bg-zinc-800 text-zinc-300 hover:bg-zinc-700"
-            }`}
-          >
-            <UserPlus />
-          </button>
+    <div data-theme="black" className="w-screen min-h-[90vh] bg-base-200 xl:flex">
+      <div className="w-11/12 xl:w-1/4 min-h-full bg-base-200 xl:border-r border-zinc-700 text-white mx-auto">
+        <div className="relative flex w-full px-5 gap-4 xl:border-b border-zinc-500 py-5 justify-center">
+          <Dock direction="middle" >
+            <div className="block xl:hidden">
+            <DockIcon
+              onClick={() => handleTabs("")}
+              className={`${tabName === "" ? "text-blue-500" : "text-white"}`}
+            >
+              <House size={24} />
+            </DockIcon>
+            </div>
+            <DockIcon
+              onClick={() => handleTabs("chat")}
+              className={`${
+                tabName === "chat" ? "text-blue-500" : "text-white"
+              }`}
+            >
+              <MessageCircle size={24} />
+            </DockIcon>
+            <DockIcon
+              onClick={() => handleTabs("connection")}
+              className={`${
+                tabName === "connection" ? "text-blue-500" : "text-white"
+              }`}
+            >
+              <UserPlus size={24}/>
+            </DockIcon>
+            <DockIcon
+              onClick={() => handleTabs("request")}
+              className={`${
+                tabName === "request" ? "text-blue-500" : "text-white"
+              }`}
+            >
+              <Users size={24}/>
+            </DockIcon>
+          </Dock>
         </div>
-        <div>
-          {tabName === "chat" && "Chats"}
+        <div className={`${tabName === "" ? "hidden" : "block"}`}>
+          {tabName === "chat" && "Coming Soon..."}
           {tabName === "connection" && <Connections />}
           {tabName === "request" && <Requests />}
         </div>
       </div>
-      <div className="w-3/4 h-full bg-transparent flex items-center justify-center relative flex-wrap gap-5">
+      <div
+        className={`w-11/12 xl:w-3/4 h-full bg-transparent flex items-center justify-center relative flex-wrap gap-5 ${
+          tabName === "" ? "block" : "hidden"
+        } xl:block mx-auto`}
+      >
         <Outlet />
       </div>
     </div>
