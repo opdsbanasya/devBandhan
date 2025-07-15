c0da32dbdc50e79398066ecff9af3ffca3aa8de5
DHARM <opdsbanasya@gmail.com>
2025-07-15 15:04:02 +0530

    handling is no request

--- frontend/src/Components/ui/Requests.jsx
+++ frontend/src/Components/ui/Requests.jsx
@@ -6,7 +6,8 @@ const Requests = () => {
   const { requests } = useSelector((store) => store.connectionRequest);
 
   return (
-    <div className="w-full md:w-3/4 xl:w-full mx-auto px-2 md:px-5 ">
+    <div className="w-full md:w-3/4 xl:w-full mx-auto px-3 py-3 md:px-5">
+      {requests && requests.length === 0 && <div>No request</div>}
       {requests &&
         requests.map((request, idx) => (
           <Request request={request} key={request._id} idx={idx} />
