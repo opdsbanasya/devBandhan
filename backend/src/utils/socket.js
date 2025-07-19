057e4788334881b7c4ab1276e1419c3cd9eeb0b7
DHARM <opdsbanasya@gmail.com>
2025-07-19 23:19:48 +0530

    handling events on server

--- backend/src/utils/socket.js
+++ backend/src/utils/socket.js
@@ -2,14 +2,29 @@ const socket = require("socket.io");
 const initilizeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5137",
+      origin: "http://localhost:5173",
     },
   });
 
   io.on("connection", (socket) => {
     // events
-    socket.on("joinChat", () => {});
-    socket.on("sendMessage", () => {});
+    socket.on("joinChat", ({ userId, toUserId, firstName }) => {
+      const roomId = [userId, toUserId].sort().join("_");
+      console.log(firstName + " Joined :" + roomId);
+      socket.join(roomId)
+    });
+
+    socket.on("sendMessage", ({userId, toUserId, firstName, newMessage}) => {
+      const roomId = [userId, toUserId].sort().join("_");
+      console.log(firstName + " Joined:" + roomId);
+      console.log({firstName, newMessage});
+      
+      io.to(roomId).emit("messageRecieved", {
+        firstName,
+        newMessage,
+      });
+    });
+
     socket.on("disconnect", () => {});
   });
 };
