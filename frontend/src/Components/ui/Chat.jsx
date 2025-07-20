699b0691a1065c7f22ec9f146a6ff8b79d64c6cf
DHARM <opdsbanasya@gmail.com>
2025-07-20 22:48:02 +0530

    fixing the base url

--- frontend/src/Components/ui/Chat.jsx
+++ frontend/src/Components/ui/Chat.jsx
@@ -1,3 +1,4 @@
+import { IMG_LINK } from "@/utils/constants";
 import { createSocketConnetion } from "@/utils/socketClient";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "/api"
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api/";
+export const IMG_LINK = "https://cdn-icons-png.flaticon.com/512/9512/9512683.png";
